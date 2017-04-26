c3de75ec3ada34cf13b54b8abb678ee5eaf68a85
stavrosbetas <wertstavros@hotmail.com>
2017-04-26 16:42:04 +0300

    creating Database.php // Connection with DB // and config file with database credentials

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+  // DB Parameters
+  define ("DB_HOST", "localhost");
+  define ("DB_USER", "root");
+  define ("DB_PASS", "");
+  define ("DB_NAME", "talkingspace");
+
+  define ("SITE_TITLE", "Welcome to talking space!");
+
+  // Paths
+  define ("BASE_URI", 'http://'.$_SERVER['SERVER_NAME'].'/talkingspace/');
+
+ ?>
