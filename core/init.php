b22680d87bebff304c88a615a3b493bb8122290d
stavrosbetas <wertstavros@hotmail.com>
2017-05-02 14:18:27 +0300

    include helpers

--- core/init.php
+++ core/init.php
@@ -10,4 +10,9 @@ function __autoload($class_name){
   require_once('libraries/' . $class_name . '.php');
 }
 
+//Helper Function Files
+require_once('helpers/system_helper.php');
+require_once('helpers/format_helper.php');
+require_once('helpers/db_helper.php');
+
 ?>
