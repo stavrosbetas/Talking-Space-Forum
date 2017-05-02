bc589f3f1e3ecce723fb1855b50d7bf999e4a5c3
stavrosbetas <wertstavros@hotmail.com>
2017-05-02 15:44:42 +0300

    finish with the templates and static html

---
+++ templates/topics.php
@@ -0,0 +1,60 @@
+<?php include('includes/header.php'); ?>
+
+              <ul id="topics">
+                <li class="topic">
+                  <div class="row">
+                    <div class="col-md-2">
+                      <img class="avatar pull-left" src="images/gravatar.jpg"/>
+                    </div>
+                    <div class="col-md-10">
+                      <div class="topic-content pull-right">
+                        <h3><a href="topic.php">How did you learn css and html</a></h3>
+                        <div class="topic-info">
+                          <a href="category.php">Development</a> >> <a href="profile.html">Brad</a>
+                          <span class="badge pull-right">3</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li class="topic">
+                  <div class="row">
+                    <div class="col-md-2">
+                      <img class="avatar pull-left" src="images/gravatar.jpg"/>
+                    </div>
+                    <div class="col-md-10">
+                      <div class="topic-content pull-right">
+                        <h3><a href="topic.php">How to create page dynamically in php</a></h3>
+                        <div class="topic-info">
+                          <a href="category.html">Development</a> >> <a href="profile.html">Brad</a>
+                          <span class="badge pull-right">3</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+                <li class="topic">
+                  <div class="row">
+                    <div class="col-md-2">
+                      <img class="avatar pull-left" src="images/gravatar.jpg"/>
+                    </div>
+                    <div class="col-md-10">
+                      <div class="topic-content pull-right">
+                        <h3><a href="topic.html">Google Panda = who's affected</a></h3>
+                        <div class="topic-info">
+                          <a href="category.html">search Engine</a> >> <a href="profile.html">Brad</a>
+                          <span class="badge pull-right">3</span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </li>
+
+              </ul>
+              <h3>Forum Statistics</h3>
+              <ul>
+                <li>Total Number of Users: <strong>52</strong></li>
+                <li>Total Number of Topics: <strong>10</strong></li>
+                <li>Total Number of Categories: <strong>5</strong></li>
+              </ul>
+<?php include('includes/footer.php'); ?>
---
+++ topics.php
@@ -0,0 +1,12 @@
+<?php require ('core/init.php'); ?>
+
+<?php
+// Get Template and Assign Vars
+  $template = new Template('templates/topics.php');
+
+//Assign Vars
+
+
+  echo $template;
+?>
+<?php include('templates/includes/header.php'); ?>
